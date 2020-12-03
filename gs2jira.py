e45109a7a2494be38a92c805ef0658589e9cec33
smart0120 <gadrgon0305@gmail.com>
2020-12-03 06:32:18 -0300

    loading params from .env

--- gs2jira.py
+++ gs2jira.py
@@ -6,17 +6,25 @@ This gist allows you to quickly create a functioning
 python3 terminal script using argparse and subprocess.
 """
 
-import gspread
+import os, gspread
+from dotenv import load_dotenv
+from jira import JIRA
 
 __author__ = "bursno22"
 __license__ = "MIT"
 __version__ = "0.0.1"
 
+load_dotenv()
 
 def main():
     gc = gspread.oauth()
-    sh = gc.open("ticket_management")
-    print(sh.sheet1.get('G8'))
+    sh = gc.open(os.getenv('SHEET_NAME'))
+    print (sh.sheet1.get('G8'))
+
+    auth_jira = JIRA(options={'server': os.getenv('JIRA_SERVER_URL')}, 
+        basic_auth=(os.getenv('JIRA_USERNAME'), os.getenv('JIRA_OAUTH_TOKEN')))
+    issue = auth_jira.issue('TIC-1')
+    print (issue.fields.summary)
 
 if __name__ == '__main__':
     main()
