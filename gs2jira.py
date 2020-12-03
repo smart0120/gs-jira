4803eb0898320d47b26e6d1cc6ad787bc24ae90d
smart0120 <gadrgon0305@gmail.com>
2020-12-03 08:45:42 -0300

    loading data from googlesheet completed

--- gs2jira.py
+++ gs2jira.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-"""Template for python3 terminal scripts.
-This gist allows you to quickly create a functioning
-python3 terminal script using argparse and subprocess.
+"""
+Script for converting google sheet rows to Jira tickets
 """
 
 import os, gspread
@@ -16,15 +15,34 @@ __version__ = "0.0.1"
 
 load_dotenv()
 
+def index_from_col(col_name):
+    return ord(col_name.upper()) - 65
+
 def main():
+    # Open Google Sheet
     gc = gspread.oauth()
     sh = gc.open(os.getenv('SHEET_NAME'))
-    print (sh.sheet1.get('G8'))
 
+    # Open JIRA
     auth_jira = JIRA(options={'server': os.getenv('JIRA_SERVER_URL')}, 
         basic_auth=(os.getenv('JIRA_USERNAME'), os.getenv('JIRA_OAUTH_TOKEN')))
-    issue = auth_jira.issue('TIC-1')
-    print (issue.fields.summary)
+
+    # Loop each row of Google sheet
+    row_range = [int(val) for val in os.getenv('DATA_RANGE').split(':')]
+
+    for row in range(row_range[0], row_range[1]+1):
+        # Read one row from google spread sheet
+        record = sh.get_worksheet(0).row_values(row)
+        cid = record[index_from_col(os.getenv('CID'))]
+        done_date = record[index_from_col(os.getenv('DONE_DATE'))]
+        jira_issue_key = record[index_from_col(os.getenv('JIRA_ISSUE_KEY'))]
+        ticket_status = record[index_from_col(os.getenv('TICKET_STATUS'))]
+        assignee = record[index_from_col(os.getenv('ASSIGNEE'))]
+
+        # check if ticket status is 'Open'
+        if (ticket_status and ticket_status.lower() == 'open'):
+            issue = auth_jira.issue(jira_issue_key)
+            print (issue.fields.summary)
 
 if __name__ == '__main__':
     main()
